09f9720f6d39aeb966c364f2816551f4f6e04477
Vijaykumar Chowdary <vijaykumar@Vijaykumars-MacBook-Air.local>
2020-07-20 20:50:27 +0530

    using click and listing all details of ec2

---
+++ shotty/shotty.py
@@ -0,0 +1,23 @@
+import boto3
+import click
+
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List_instances"
+    for i in ec2.instances.all():
+        print(','.join((
+        i.id,
+        i.instance_type,
+        i.placement['AvailabilityZone'],
+        i.state['Name'],
+        i.public_dns_name)))
+
+    return
+
+if __name__ == '__main__':
+
+    list_instances()
